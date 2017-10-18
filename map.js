e84bb29b6824acdff027e843adcf15cf84259d5a
Carlos <carlosgarcia21pinos@gmail.com>
2017-10-18 09:19:04 +0200

    Geolocalizacion añadida

--- map.js
+++ map.js
@@ -1,6 +1,22 @@
-var map = L.map('map').setView([37.1880, -3.7180], 15);
+var map = L.map('map').setView([37.1880, -3.7180], 9);
 
 var mapa = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap<\/a> contributors'
 });
 mapa.addTo(map);
+
+function onLocationFound(e) {
+         var radius = e.accuracy / 2;
+         var location = e.latlng
+         L.marker(location).addTo(map)
+         L.circle(location, radius).addTo(map);
+      }
+      function onLocationError(e) {
+         alert(e.message);
+      }
+      function getLocationLeaflet() {
+         map.on('locationfound', onLocationFound);
+         map.on('locationerror', onLocationError);
+         map.locate({setView: true, maxZoom: 10});
+      }
+
